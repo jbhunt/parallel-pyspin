a9b49fed4935171ce2982b357891ded9ca580de2
jbhunt <hunt.brian.joshua@gmail.com>
2020-07-21 19:19:28 -0600

    renamed the children.py module to processes.py

--- llpyspin/processes.py
+++ llpyspin/processes.py
@@ -9,31 +9,8 @@ from multiprocessing import Queue
 from multiprocessing import Process
 from multiprocessing import Lock
 
-# relative imports
-
-# image properties
-from .constants import (
-    IMAGE_SIZE,
-    IMAGE_SHAPE,
-    IMAGE_WIDTH,
-    IMAGE_HEIGHT
-    )
-
-# default acquisition property key and value pairs
-from .constants import (
-    CAP_PROP_FPS,
-    CAP_PROP_FPS_DEFAULT,
-    CAP_PROP_BINSIZE,
-    CAP_PROP_BINSIZE_DEFAULT,
-    CAP_PROP_EXPOSURE,
-    CAP_PROP_EXPOSURE_DEFAULT,
-    CAP_PROP_WIDTH,
-    CAP_PROP_WIDTH_DEFAULT,
-    CAP_PROP_HEIGHT,
-    CAP_PROP_HEIGHT_DEFAULT,
-    CAP_PROP_OFFSET,
-    CAP_PROP_OFFSET_DEFAULT
-    )
+# constants
+import .constants as c
 
 # logging setup
 logging.basicConfig(format='%(levelname)s : %(message)s',level=logging.INFO)
@@ -44,9 +21,8 @@ try:
 except ModuleNotFoundError:
     logging.error('PySpin import failed.')
 
-class ChildProcess(Process):
+class BaseProcess(Process):
     """
-    child process
     """
 
     def __init__(self, device):
@@ -141,14 +117,8 @@ class ChildProcess(Process):
 
         return
 
-    def _initialize(self, camera, mode='NewestOnly'):
+    def _initialize(self, camera):
         """
-        keywords
-        --------
-        camera
-            instance of a camera object
-        mode
-            stream buffer handling mode
         """
 
         # check that the mode argument is a valid stream buffer handling mode
@@ -164,14 +134,6 @@ class ChildProcess(Process):
 
             #
             camera.Init()
-
-            # set the stream bufer handling mode to oldest first
-            tlstream_nodemap = camera.GetTLStreamNodeMap()
-            handling_mode = PySpin.CEnumerationPtr(tlstream_nodemap.GetNode('StreamBufferHandlingMode'))
-            if PySpin.IsAvailable(handling_mode) and PySpin.IsWritable(handling_mode):
-                handling_mode_oldest_first = handling_mode.GetEntryByName(mode)
-                if PySpin.IsAvailable(handling_mode_oldest_first) and PySpin.IsReadable(handling_mode_oldest_first):
-                    handling_mode.SetIntValue(handling_mode_oldest_first.GetValue())
 
             # set the acquisition mode to continuous
             nodemap = camera.GetNodeMap()
@@ -205,8 +167,13 @@ class ChildProcess(Process):
 
         # check that the property is valid
         try:
-            assert property in [CAP_PROP_FPS,CAP_PROP_BINSIZE,CAP_PROP_EXPOSURE]
-        except:
+            assert property in [
+                c.CAP_PROP_FPS,
+                c.CAP_PROP_BINSIZE,
+                c.CAP_PROP_EXPOSURE,
+                c.CAP_PROP_BUFFER_HANDLING_MODE
+                ]
+        except AssertionError:
             result = False
             return result
 
@@ -214,18 +181,18 @@ class ChildProcess(Process):
         try:
 
             # set binsize
-            if property == 'binsize':
+            if property == c.CAP_PROP_BINSIZE:
                 camera.BinningHorizontal.SetValue(value)
                 camera.BinningVertical.SetValue(value)
 
             # set exposure
-            elif property == 'exposure':
+            elif property == c.CAP_PROP_EXPOSURE:
                 camera.AcquisitionFrameRateEnable.SetValue(False)
                 camera.ExposureAuto.SetValue(PySpin.ExposureAuto_Off)
                 camera.ExposureTime.SetValue(value)
 
             # set framerate
-            elif property == 'framerate':
+            elif property == c.CAP_PROP_FPS:
 
                 # enable framerate customization
                 camera.AcquisitionFrameRateEnable.SetValue(True)
@@ -241,21 +208,20 @@ class ChildProcess(Process):
                 else:
                     camera.AcquisitionFrameRate.SetValue(value)
 
+            # set buffer handling mode
+            elif property == c.CAP_PROP_BUFFER_HANDLING_MODE:
+
+                tlstream_nodemap = camera.GetTLStreamNodeMap()
+                handling_mode = PySpin.CEnumerationPtr(tlstream_nodemap.GetNode('StreamBufferHandlingMode'))
+                if PySpin.IsAvailable(handling_mode) and PySpin.IsWritable(handling_mode):
+                    handling_mode_value = handling_mode.GetEntryByName(value)
+                    if PySpin.IsAvailable(handling_mode_oldest_first) and PySpin.IsReadable(handling_mode_value):
+                        handling_mode.SetIntValue(handling_mode_value.GetValue())
+
         except PySpin.SpinnakerException:
             result = False
 
         return result
-
-    def _acquire(self, camera):
-        """
-        start video acquisition
-
-        notes
-        -----
-        This method will be unique for each subclass so it is defined only in name here
-        """
-
-        return True
 
     def _deacquire(self, camera):
         """
@@ -280,11 +246,11 @@ class ChildProcess(Process):
 
         try:
 
-            # TODO: Figure out why the camera is still streaming after the _deacquire method is called
-            # this is a temporary fix
+            # TODO: figure out why the camera is still streaming after the _deacquire method is called
+
             if camera.IsStreaming():
                 camera.EndAcquisition()
-                
+
             camera.DeInit()
 
         except:
@@ -292,7 +258,7 @@ class ChildProcess(Process):
 
         return result
 
-class VideoStreamChildProcess(ChildProcess):
+class VideoStreamProcess(BaseProcess):
     """
     """
 
@@ -347,3 +313,139 @@ class VideoStreamChildProcess(ChildProcess):
             result = False
 
         return result
+
+class PrimaryCameraProcess(BaseProcess):
+    """
+    """
+
+    def __init__(self, device):
+        """
+        """
+
+        super().__init__(device)
+
+        self.triggered = Value('i',0)
+
+        return
+
+    def _configure(self, camera):
+        """
+        """
+
+        result = True
+
+        try:
+
+            # counter 0 tracks the start of exposure
+            camera.CounterSelector.SetValue(PySpin.CounterSelector_Counter0)
+            camera.CounterEventSource.SetValue(PySpin.CounterEventSource_ExposureStart)
+            camera.CounterTriggerSource.SetValue(PySpin.CounterTriggerSource_ExposureStart)
+            camera.CounterTriggerActivation.SetValue(PySpin.CounterTriggerActivation_RisingEdge)
+
+            # create a digital signal at half the frequency of the framerate and 50% duty cycle
+            camera.LineSelector.SetValue(PySpin.LineSelector_Line2)
+            camera.V3_3Enable.SetValue(True)
+            camera.LineSelector.SetValue(PySpin.LineSelector_Line1)
+            camera.LineSource.SetValue(PySpin.LineSource_Counter0Active)
+
+            # set the trigger to be a software input
+            camera.TriggerMode.SetValue(PySpin.TriggerMode_Off)
+            camera.TriggerSource.SetValue(PySpin.TriggerSource_Software)
+            camera.TriggerOverlap.SetValue(PySpin.TriggerOverlap_Off)
+            camera.TriggerMode.SetValue(PySpin.TriggerMode_On)
+
+        except:
+            result = False
+
+        return result
+
+    def _deconfigure(self, camera):
+        """
+        """
+
+        result = True
+
+        try:
+            camera.TriggerMode.SetValue(PySpin.TriggerMode_On)
+            camera.LineSelector.SetValue(PySpin.LineSelector_Line1)
+            camera.LineSource.SetValue(PySpin.LineSource_FrameTriggerWait)
+            camera.LineInverter.SetValue(True)
+
+        except:
+            result = False
+
+        return result
+
+    def _acquire(self, camera):
+        """
+        """
+
+        result = True
+
+        try:
+            camera.BeginAcquisition()
+            camera.LineSelector.SetValue(PySpin.LineSelector_Line1)
+            camera.LineSource.SetValue(PySpin.LineSource_Counter0Active)
+
+            # double-check that the acquisition flag is set to 1
+            try:
+                assert self.acquiring.value == 1
+            except AssertionError:
+                self.acquiring.value = 1
+
+            # wait for the trigger
+            while not self.triggered.value:
+                continue
+
+            # release the trigger
+            camera.TriggerMode.SetValue(PySpin.TriggerMode_Off)
+
+            # main loop
+            while self.acquiring.value == 1:
+
+                image = camera.GetNextImage()
+
+                #
+                if not image.IsIncomplete():
+
+                    # convert the image
+                    frame = image.Convert(PySpin.PixelFormat_Mono8, PySpin.HQ_LINEAR)
+
+
+        except:
+            result = False
+
+        # reset the trigger flag
+        self.triggered.value = 0
+
+        return result
+
+class SecondaryCameraProcess(BaseProcess):
+    """
+    """
+
+    def __init__(self, device):
+        """
+        """
+
+        super().__init__(device)
+
+        return
+
+    def _configure(self, camera):
+        """
+        """
+
+        return
+
+    def _deconfigure(self, camera):
+        """
+        """
+
+        return
+
+    def _acquire(self):
+        """
+        """
+
+        return

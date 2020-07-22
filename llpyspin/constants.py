1ae0f685bcc3b2a2faa9a534376a582b78823546
jbhunt <hunt.brian.joshua@gmail.com>
2020-07-21 19:19:55 -0600

    added a list of all valid capture properties to the constants module

--- llpyspin/constants.py
+++ llpyspin/constants.py
@@ -20,7 +20,7 @@ IMAGE_HEIGHT = CONFIG_DATA['cameras']['primary']['resolution'][1]
 IMAGE_SIZE   = IMAGE_WIDTH * IMAGE_HEIGHT
 IMAGE_SHAPE  = (IMAGE_HEIGHT,IMAGE_WIDTH) # convenient for shaping numpy arrays
 
-# capture properties
+# basic capture properties
 CAP_PROP_FPS      = 'framerate'
 CAP_PROP_BINSIZE  = 'binsize'
 CAP_PROP_EXPOSURE = 'exposure'
@@ -28,10 +28,24 @@ CAP_PROP_WIDTH    = 'width'
 CAP_PROP_HEIGHT   = 'height'
 CAP_PROP_OFFSET   = 'offset'
 
-# default capture properties
+# default values for the basic capture properties
 CAP_PROP_FPS_DEFAULT      = 60
 CAP_PROP_BINSIZE_DEFAULT  = 2
 CAP_PROP_EXPOSURE_DEFAULT = 1500
 CAP_PROP_WIDTH_DEFAULT    = IMAGE_WIDTH
 CAP_PROP_HEIGHT_DEFAULT   = IMAGE_HEIGHT
 CAP_PROP_OFFSET_DEFAULT   = (0,0)
+
+# buffer handling mode property and valid modes
+CAP_PROP_BUFFER_HANDLING_MODE           = 'mode'
+CAP_PROP_BUFFER_HANDLING_MODE_DEFAULT   = 'MostRecentFirst'
+CAP_PROP_BUFFER_HANDLING_MODE_STREAMING = 'NewestOnly'
+CAP_PROP_BUFFER_HANDLING_MODE_RECORDING = 'MostRecentFirst'
+
+#
+VALID_CAP_PROPS = (
+    CAP_PROP_FPS,
+    CAP_PROP_BINSIZE,
+    CAP_PROP_EXPOSURE,
+    CAP_PROP_BUFFER_HANDLING_MODE
+    )

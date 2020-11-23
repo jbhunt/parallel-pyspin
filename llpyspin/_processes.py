46da03e77edc83e02637f3160c78d4f9c0b70fd0
jbhunt <hunt.brian.joshua@gmail.com>
2020-11-23 12:43:15 -0700

    refactored the streaming class

--- llpyspin/_processes.py
+++ llpyspin/_processes.py
@@ -1,6 +1,7 @@
 import dill
 import types
 import queue
+import PySpin
 import logging
 import numpy as np
 import multiprocessing as mp
@@ -11,28 +12,7 @@ ACQUIRING = mp.Value('i', 0)
 # logging setup
 logging.basicConfig(format='%(levelname)s : %(message)s',level=logging.INFO)
 
-# try to import the PySpin package
-try:
-    import PySpin
-except ModuleNotFoundError:
-    logging.error('PySpin import failed.')
-
-class SubprocessError(Exception):
-    """error raised for failed attempt to create child process"""
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
-        return
-
-class PropertyError(Exception):
-    """error raised for failed attempts to set the value of an acquisition property"""
-
-    def __init__(self, property, value):
-        super().__init__()
-        self.message = f'{value} is not a valid value for {property}'
-        return
-
-class CameraBaseV2(mp.Process):
+class CameraBase(mp.Process):
     """
     """
 
@@ -66,6 +46,9 @@ class CameraBaseV2(mp.Process):
             self._acquiring = kwargs['acquiring']
         else:
             self._acquiring = ACQUIRING
+
+        #
+        self._lock = mp.Lock()
 
         # parameters (determined during initialization)
         self._framerate = None
@@ -395,252 +378,17 @@ class CameraBaseV2(mp.Process):
 
         return value
 
-class CameraBase():
-    """
-    this is the base class for any subclass of camera which handles the creation,
-    operation, and destruction of the child process
-    """
-
-    def __init__(self, device):
-        """
-        keywords
-        --------
-        device : int or str
-            the camera's index or serial number
-        """
-
-        self.device = device
-
-        # global variables
-        global _ACQUIRING
-
-        # private attributes
-
-        self._child           = None            # attribute which holds the reference to the child process
-
-        self._started         = mp.Value('i',0) # this flag controls the main loop in the run method
-        # self._acquiring       = mp.Value('i',0) # this flag controls the acquisition loop in the _start method
-        self._acquiring       = _ACQUIRING
-
-        self._lock            = mp.Lock()       # acquisition lock
-        self._locked          = False           # acquisition lock state
-
-        self._iq              = mp.Queue()      # input queue
-        self._oq              = mp.Queue()      # output queue
-
-        # acquisition property private values
-        self._framerate = None
-        self._exposure  = None
-        self._binsize   = None
-        self._roi       = None
-
-        # alternative naming
-        # self._fps             = None
-        # self._bin             = None
-        # self._exp             = None
-
-        return
-
-    # TODO : the more I think about this, the more I think the child process
-    #        should be made into its own class
-
-    def _run(self):
-        """
-        target function for the child process
-
-        notes
-        -----
-        Do not make logging calls (e.g., 'logging.info(<some informative message>)')
-        within this method. Writing to stdout is not a process-safe operation.
-        """
-
-        try:
-
-            # create instances of the system and cameras list
-            system  = PySpin.System.GetInstance()
-            cameras = system.GetCameras()
-
-            #
-            assert len(cameras) != 0
-
-            # instantiate the camera
-            if type(self.device) == str:
-                camera = cameras.GetBySerial(self.device)
-
-            if type(self.device) == int:
-                camera = cameras.GetByIndex(self.device)
-
-            # send the result back
-            self._oq.put(True)
-
-        except:
-
-            # clean-up
-            try:
-                del camera
-            except NameError:
-                pass
-            cameras.Clear()
-            del cameras
-            system.ReleaseInstance()
-
-            # send the result back
-            self._oq.put(False)
-
-            return
-
-        # set the started flag to True
-        self.started = True
-
-        # main loop
-        while self.started:
-
-            # listen for method calls from the main process
-            try:
-                item = self._iq.get(block=False)
-                # print(item)
-
-            except queue.Empty:
-                continue
-
-            # call the appropriate method
-            try:
-                method = self.__getattribute__('_' + item)
-                method(camera)
-                self._oq.put(True)
-
-            # except PySpin.SpinnakerException:
-            except:
-                self._oq.put(False)
-
-            continue
-
-        # clean up
-        try:
-            del camera
-        except NameError:
-            pass
-        cameras.Clear()
-        del cameras
-        system.ReleaseInstance()
-
-        return
-
-    def _spawn(self):
-        """
-        spawn the child process
-        """
-
-        if self._child != None:
-            logging.debug('active child process detected')
-            return
-
-        logging.debug('creating the child process')
-
-        # create the child process
-        self._child = mp.Process(target=self._run,args=())
-
-        # make the child a daemon process
-        self._child.daemon = True
-
-        # start the child process
-        self._child.start()
-
-        # the child will send back the result of the fork
-        result = self._oq.get()
-        if result == False:
-            raise SubprocessError('failed to spawn child process')
-
-        return
-
-    def _kill(self):
-        """
-        kill the child process
-        """
-
-        if self._child == None:
-            logging.debug('no active child process detected')
-            return
-
-        logging.debug('destroying the child process')
-
-        # break out of the main loop in the child process
-        self.started = False
-
-        # empty out the queues - if the are not empty it can cause the call to the join method to hang
-        if self._iq.qsize() != 0 or self._oq.qsize() != 0:
-            logging.debug('emptying input and output queues')
-            while not self._iq.empty(): item = self._iq.get()
-            while not self._oq.empty(): item = self._oq.get()
-
-        # join the child process
-        self._child.join(timeout=15)
-        if self._child.is_alive():
-            logging.error('child process is deadlocked')
-            self._child.terminate()
-            self._child.join()
-
-        # delete the reference to the child process
-        self._child = None
-
-        return
-
-    # NOTE : this method is not yet implemented
-    # TODO : implement this method
-
-    def _send(self, item, caller='parent', *items):
-        """
-        put one or more items in the input queue
-
-        keywords
-        --------
-        caller : str
-            identity of the process (i.e., parent or child)
-
-        notes
-        -----
-        The caller kwarg specifies which queue to put the item(s) in.
-        """
-
-        if caller == 'parent':
-            q = self._iq
-        elif caller == 'child':
-            q = self._oq
-        else:
-            raise ValueError('invalid caller-ID')
-
-        q.put(item)
-
-        # option to send follow up data
-        if len(items) != 0:
-            q.put(items)
-
-        return
-
-    # retreive the result of a call from the output queue
+    # width
     @property
-    def _result(self):
+    def width(self):
+        x, y, w, h = self.roi
+        return w
 
-        # try:
-        #     result = self._oq.get(block=True, timeout=5)
-
-        # except mp.TimeoutError:
-        #     raise SubprocessError('failed to retreive result from output queue')
-
-        # except queue.Empty:
-        #     raise SubprocessError('failed to retreive result from output queue')
-
-        result = self._oq.get()
-
-        return result
-
-    # started flag
+    # height
     @property
-    def started(self): return True if self._started.value == 1 else False
-
-    @started.setter
-    def started(self, value):
-        self._started.value = 1 if value == True else False
+    def height(self):
+        x, y, w, h = self.roi
+        return h
 
     # acquiring flag
     @property
@@ -653,7 +401,7 @@ class CameraBase():
     # acquisition lock state
     @property
     def locked(self):
-        return self._locked
+        return True if self._lock.locked else False
 
     @locked.setter
     def locked(self, value):
--- llpyspin/streaming.py
+++ llpyspin/streaming.py
@@ -17,9 +17,8 @@ try:
 except ModuleNotFoundError:
     logging.error('PySpin import failed.')
 
-class VideoStream(CameraBase, SpinnakerMixin, PropertiesMixin):
+class VideoStream(CameraBase):
     """
-    a class for streaming video, i.e., where recording is not the target function
     """
 
     def __init__(self, device=0):
@@ -28,150 +27,85 @@ class VideoStream(CameraBase, SpinnakerMixin, PropertiesMixin):
 
         super().__init__(device)
 
-        # this bit of memory stores the image
-        self._container = mp.Array('i',3000000)
-
-        # open the stream
-        self.open()
+        x, y, w, h = self.roi
+        self._container = mp.Array('i', w * h)
 
         return
-
-    def _start(self, camera):
-        """
-        start acquisition
-        """
-
-        # engage the acquisition lock
-        camera.BeginAcquisition()
-
-        # main acquisition loop
-        while self.acquiring:
-
-            image = camera.GetNextImage(1000)
-
-            #
-            if not image.IsIncomplete():
-
-                # convert the image
-                frame = image.Convert(PySpin.PixelFormat_Mono8, PySpin.HQ_LINEAR)
-                array = frame.GetNDArray().flatten()
-                image = np.zeros_like(self._container[:])
-                image[:array.size] = array
-
-                # store the image (critical - use lock)
-                with self._container.get_lock():
-                    self._container[:] = image
-
-        return
-
-    ### public methods ###
-
-    def isOpened(self):
-        """
-        """
-
-        # TODO : figure out if I really need to check the first condition
-
-        result = True if self._child != None and self.acquiring == True else False
-
-        return result
 
     def open(self):
         """
-        open the stream
         """
 
-        #
-        if self.acquiring:
-            logging.info('video stream is already open')
-            return
-
-        logging.info('opening the video stream')
-
-        # spawn the child process
-        self._spawn()
-
-        # initialize the camera
-        self._iq.put('initialize')
-        if self._result == False:
-            logging.debug(f'camera initialization failed')
-            self._destroy()
-
-        # set all properties to their default values
-        self.framerate = None
-        self.exposure  = None
-        self.binsize   = None # this will also set the roi
-
-        # set the acquiring flag
+        # set the acquisition flag
         self.acquiring = True
 
-        # start the video acquisition
-        self._iq.put('start')
+        def f(camera):
+
+            try:
+                camera.BeginAcquisition()
+
+                # main acquisition loop
+                while self.acquiring:
+
+                    try:
+                        image = camera.GetNextImage(1)
+                    except PySpin.SpinnakerException:
+                        continue
+
+                    #
+                    if not image.IsIncomplete():
+
+                        # convert the image
+                        data = image.Convert(PySpin.PixelFormat_Mono8, PySpin.HQ_LINEAR).GetNDArray().flatten()
+
+                        # store the image (critical - use lock)
+                        with self._container.get_lock():
+                            self._container[:] = image
+            except:
+                return False
+
+            return True
+
+        #
+        self._iq.put(dill.dumps(f))
 
         return
 
-    def release(self):
+    def close(self):
         """
         """
 
-        if not self.acquiring:
-            logging.info('video stream is closed')
-            return
-
-        logging.info('releasing the video stream')
-
-        # break out of the acquisition loop
         self.acquiring = False
 
-        # retreive the result (sent after exiting the acquisition loop)
-        if self._result == False:
-            logging.debug('video acquisition failed')
-
-        # stop acquisition
-        self._iq.put('stop')
-        if self._result == False:
-            logging.debug('video de-acquisition failed')
-
-        # release camera
-        self._iq.put('release')
-        if self._result == False:
-            logging.debug('camera de-initialization failed')
-
-        # kill the child process
-        self._kill()
+        result = self._oq.get()
+        if not result:
+            logging.log(logging.ERROR, f'acquisition for camera[{self._device}] failed')
 
         return
 
     def read(self):
         """
-        get the most recently acquired image
         """
 
         if not self.acquiring:
-            logging.info('stream is closed')
+            logging.log(logging.INFO, 'stream is closed')
             return (False, None)
 
         # the lock blocks if a new image is being written to the image attribute
         with self._container.get_lock():
-            array = np.array(self._container[:],dtype=np.uint8)
-            index = np.arange(self.size,array.size)
-            if len(index) != 0:
-                array = np.delete(array, index)
-            image = array.reshape((self.height,self.width))
+            image = np.array(self._container[:], dtype=np.uint8).reshape([self.height, self.width])
 
         return (True, image)
 
-    @property
-    def size(self):
-        (i, j, h, w) = self.roi
-        return w * h
+    # extend the setter of the roi descriptor
+    @CameraBase.roi.setter
+    def roi(self, value):
 
-    @property
-    def height(self):
-        (i, j, h, w) = self.roi
-        return h
+        # invoke the setter
+        CameraBase.roi.fset(self, value)
 
-    @property
-    def width(self):
-        (i, j, h, w) = self.roi
-        return w
+        # recreate the container with the new roi
+        x, y, w, h = self.roi
+        self._container = mp.Array('i', w * h)
+
+        return

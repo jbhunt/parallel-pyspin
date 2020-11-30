fac3b326f0c7d28177e1950973fdbf09b8ccdf3d
jbhunt <hunt.brian.joshua@gmail.com>
2020-11-30 16:03:49 -0700

    updated the package version to 0.2.dev4

---
+++ build/lib/llpyspin/_primary.py
@@ -0,0 +1,259 @@
+# imports
+import dill
+import PySpin
+import logging
+import numpy as np
+import multiprocessing as mp
+
+# relative imports
+from ._processes  import MainProcess, ChildProcess, ChildProcessError
+from ._recording  import VideoWriterFFmpeg, VideoWriterSpinnaker
+
+# logging setup
+logging.basicConfig(format='%(levelname)s : %(message)s',level=logging.INFO)
+
+class ChildProcessPrimary(ChildProcess):
+    """
+    """
+
+    def __init__(self, device):
+        """
+        """
+
+        # acquisition start trigger
+        self.trigger = mp.Event()
+
+        super().__init__(device)
+
+        return
+
+
+class PrimaryCamera(MainProcess):
+    """
+    """
+
+    def __init__(self, device):
+        """
+        """
+
+        super().__init__(device)
+
+        # override the child process class specification
+        self._childClass = ChildProcessPrimary
+
+        self.open()
+
+        return
+
+    def open(self):
+        """
+        """
+
+        # spawn a child process as needed
+        if self._child == None:
+            self._initialize()
+
+        # set the buffer handling mode to oldest first (instead of newest only)
+        def f(obj, camera, *args, **kwargs):
+            try:
+                camera.TLStream.StreamBufferHandlingMode.SetValue(PySpin.StreamBufferHandlingMode_OldestFirst)
+                return True
+            except PySpin.SpinnakerException:
+                return False
+
+        item = (dill.dumps(f), [], {})
+        self._child.iq.put(item)
+        result = self._child.oq.get()
+        if not result:
+            logging.log(logging.ERROR, f'failed to set the buffer handling mode')
+
+        #
+        self._primed = False
+
+        return
+
+    def prime(self, filename, bitrate=1000000, backend='ffmpeg'):
+        """
+        """
+
+        if self._child == None:
+            raise ChildProcessError('no active child process')
+
+        if self.primed:
+            logging.log(logging.WARNING, f'camera[{self._device}] is already primed')
+
+        if backend not in ['ffmpeg', 'spinnaker']:
+            raise ValueError(f'{backend} is not a valid backend for writing video')
+
+        def f(obj, camera, *args, **kwargs):
+
+            try:
+                # create a counter that tracks the onset sensor exposure
+                camera.CounterSelector.SetValue(PySpin.CounterSelector_Counter0)
+                camera.CounterEventSource.SetValue(PySpin.CounterEventSource_ExposureStart)
+                camera.CounterTriggerSource.SetValue(PySpin.CounterTriggerSource_ExposureStart)
+                camera.CounterTriggerActivation.SetValue(PySpin.CounterTriggerActivation_RisingEdge)
+
+                # create a digital signal whose PWD is determined by the counter
+                camera.LineSelector.SetValue(PySpin.LineSelector_Line2)
+                camera.V3_3Enable.SetValue(True)
+                camera.LineSelector.SetValue(PySpin.LineSelector_Line1)
+                camera.LineSource.SetValue(PySpin.LineSource_Counter0Active)
+
+                #
+                camera.TriggerMode.SetValue(PySpin.TriggerMode_Off)
+                camera.TriggerSource.SetValue(PySpin.TriggerSource_Software)
+                camera.TriggerOverlap.SetValue(PySpin.TriggerOverlap_Off)
+                camera.TriggerMode.SetValue(PySpin.TriggerMode_On)
+
+                return True
+
+            except PySpin.SpinnakerException:
+                return False
+
+        item = (dill.dumps(f), [], {})
+        self._child.iq.put(item)
+        result = self._child.oq.get()
+        if not result:
+            logging.log(logging.ERROR, f'failed to prime camera[{self._device}]')
+            return
+
+        def f(obj, camera, *args, **kwargs):
+
+            # try:
+
+            # begin acquisition
+            camera.BeginAcquisition()
+
+            # initialize the video writer
+            if kwargs['backend'] == 'ffmpeg':
+                writer = VideoWriterFFmpeg()
+            elif kwargs['backend'] == 'spinnaker':
+                writer = VideoWriterSpinnaker()
+            else:
+                return False
+            writer.open(kwargs['filename'], kwargs['shape'], kwargs['framerate'], kwargs['bitrate'])
+
+            # wait for the trigger event
+            obj.trigger.wait()
+
+            # unset the trigger mode
+            camera.TriggerMode.SetValue(PySpin.TriggerMode_Off)
+
+            # main acquisition loop
+            while obj.acquiring.value:
+
+                try:
+                    pointer = camera.GetNextImage(1)
+                except PySpin.SpinnakerException:
+                    continue
+
+                if not pointer.IsIncomplete():
+                    writer.write(pointer)
+
+                pointer.Release()
+
+
+            # reset the trigger mode
+            camera.TriggerMode.SetValue(PySpin.TriggerMode_On)
+
+            #
+            writer.close()
+
+            return True
+
+            # except PySpin.SpinnakerException:
+            #     return False
+
+        kwargs = {
+            'filename'  : filename,
+            'shape'     : (self.height, self.width),
+            'framerate' : self.framerate,
+            'bitrate'   : bitrate,
+            'backend'   : backend
+        }
+        item = (dill.dumps(f), [], kwargs)
+        self._child.iq.put(item)
+
+        #
+        self._primed = True
+        self._locked = True
+
+        #
+        self._child.acquiring.value = 1
+
+        return
+
+    def trigger(self):
+        """
+        """
+
+        if not self._primed:
+            logging.log(logging.INFO, f'camera[{self._device}] is not primed')
+            return
+
+        self._child.trigger.set()
+
+        return
+
+    def stop(self):
+        """
+        """
+
+        if self._child == None:
+            raise ChildProcessError('no active child process')
+
+        if not self._primed:
+            logging.log(logging.INFO, f'camera[{self._device}] is not primed')
+            return
+
+        # stop acquisition
+        self._child.acquiring.value = 0
+
+        # release the trigger (in the case of abortion before the trigger is set)
+        if not self._child.trigger.is_set():
+            self._child.trigger.set()
+
+        # query the result of video acquisition
+        result = self._child.oq.get()
+        if not result:
+            logging.log(logging.ERROR, f'video acquisition for camera[{self._device}] failed')
+
+        # end acquisition and reset the camera
+        def f(obj, camera, *args, **kwargs):
+            try:
+                camera.EndAcquisition()
+                camera.TriggerMode.SetValue(PySpin.TriggerMode_On)
+                camera.LineSelector.SetValue(PySpin.LineSelector_Line1)
+                camera.LineSource.SetValue(PySpin.LineSource_FrameTriggerWait)
+                camera.LineInverter.SetValue(True)
+                return True
+            except PySpin.SpinnakerException:
+                return False
+
+        item = (dill.dumps(f), [], {})
+        self._child.iq.put(item)
+        result = self._child.oq.get()
+        if not result:
+            logging.log(logging.ERROR, f'video deacquisition for camera[{self._device}] failed')
+
+        self._primed = False
+        self._locked = False
+
+        return
+
+    def close(self):
+        """
+        """
+
+        if self._child == None:
+            raise ChildProcessError('no active child process')
+
+        self._release()
+
+        return
+
+    #
+    @property
+    def primed(self):
+        return self._primed
---
+++ build/lib/llpyspin/_streaming.py
@@ -0,0 +1,189 @@
+import dill
+import queue
+import PySpin
+import logging
+import numpy as np
+import multiprocessing as mp
+
+# relative imports
+from ._processes  import MainProcess, ChildProcess, ChildProcessError
+
+# logging setup
+logging.basicConfig(format='%(levelname)s : %(message)s',level=logging.INFO)
+
+#
+class ChildProcessStreaming(ChildProcess):
+    """
+    """
+
+    def __init__(self, device):
+        """
+        """
+
+        # create a shared memory array for storing a single image
+        # self.buffer = self._createBuffer(device)
+        self.buffer = mp.Array('i', 10 * 1000000)
+
+        #
+        super().__init__(device)
+
+        return
+
+    def _createBuffer(self, device):
+        """
+        this method determines the size of the image buffer on instantiation
+        """
+
+        #
+        try:
+            system  = PySpin.System.GetInstance()
+            cameras = system.GetCameras()
+            if type(device) == str:
+                camera = cameras.GetBySerial(device)
+            else:
+                camera = cameras.GetByIndex(device)
+            camera.Init()
+            w = camera.Width.GetMax()
+            h = camera.Height.GetMax()
+            camera.DeInit()
+            size = int(w * h)
+
+            #
+            del w; del h
+            del camera
+            cameras.Clear()
+            del cameras
+            system.ReleaseInstance()
+            del system
+
+            return mp.Array('i', size)
+
+        # NOTE - If the above code block fails, the buffer size defaults to 25
+        # million pixels which should support up to the 244S8 Blackfly S USB3
+        except PySpin.SpinnakerException:
+            return mp.Array('i', 25 * 1000000)
+
+class VideoStream(MainProcess):
+    """
+    """
+
+    def __init__(self, device=0):
+        """
+        """
+
+        super().__init__(device)
+
+        # override the child process class specification
+        self._childClass = ChildProcessStreaming
+
+        # initialize the camera and open the stream
+        self.open()
+
+        return
+
+    def open(self):
+        """
+        """
+
+        # spawn a child process as needed
+        if self._child == None:
+            self._initialize()
+
+        # return if the stream is already open
+        if self._child.acquiring.value:
+            return
+
+        # set the acquisition flag
+        self._child.acquiring.value = 1
+
+        def f(obj, camera, *args, **kwargs):
+
+            # unpack the kwargs
+            shape = kwargs['shape']
+
+            # size of the image (i.e., total number of pixels)
+            size = shape[0] * shape[1]
+
+            try:
+                camera.BeginAcquisition()
+
+                # main acquisition loop
+                while obj.acquiring.value:
+
+                    try:
+                        image = camera.GetNextImage(1)
+                    except PySpin.SpinnakerException:
+                        continue
+
+                    #
+                    if not image.IsIncomplete():
+
+                        # convert the image
+                        data = image.Convert(PySpin.PixelFormat_Mono8, PySpin.HQ_LINEAR).GetNDArray().flatten()
+
+                        # store the image (critical - use lock)
+                        with obj.buffer.get_lock():
+                            obj.buffer[:size] = data
+
+                return True
+
+            except PySpin.SpinnakerException:
+               return False
+
+        # pack the kwargs
+        kwargs = {'shape' : (self.height, self.width)}
+        item = (dill.dumps(f), [], kwargs)
+        self._child.iq.put(item)
+
+        self._locked = True
+
+        return
+
+    def close(self):
+        """
+        """
+
+        # return if there is no active child or the stream is already closed
+        if self._child == None or self._child.acquiring.value == 0:
+            return
+
+        self._child.acquiring.value = False
+
+        result = self._child.oq.get()
+        if not result:
+            logging.log(logging.ERROR, f'acquisition for camera[{self._device}] failed')
+
+        def f(obj, camera, *args, **kwargs):
+            try:
+                camera.EndAcquisition()
+                return True
+            except:
+                return False
+
+        item = (dill.dumps(f), [], {})
+        self._child.iq.put(item)
+        result = self._child.oq.get()
+        if not result:
+            logging.log(logging.ERROR, f'video deacquisition for camera[{self._device}] failed')
+
+        self._locked = False
+
+        # release the camera
+        self._release()
+
+        return
+
+    def read(self):
+        """
+        """
+
+        # return if there is no active child or the stream is closed
+        if self._child == None or self._child.acquiring.value == 0:
+            return
+
+        # the lock blocks if a new image is being written to the image attribute
+        with self._child.buffer.get_lock():
+            data = self._child.buffer[:][:self.width * self.height]
+            image = np.array(data, dtype=np.uint8).reshape([self.height, self.width])
+
+        return (True, image)

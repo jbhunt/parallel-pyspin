36a280092c3a0a118e4f61f3a5038390812ab736
jbhunt <hunt.brian.joshua@gmail.com>
2021-07-08 13:05:23 -0600

    refactored the streaming module

--- llpyspin/streaming.py
+++ llpyspin/streaming.py
@@ -1,7 +1,5 @@
 import dill
-import queue
 import PySpin
-import logging
 import numpy as np
 import multiprocessing as mp
 
@@ -17,48 +15,13 @@ class StreamingChildProcess(ChildProcess):
         """
         """
 
-        # create a shared memory array for storing a single image
-        # self.buffer = self._initialize_image_buffer(device)
-        self.buffer = mp.Array('i', 10 * 1000000)
+        # create a shared memory list for buffering a single image
+        self.buffer = mp.Manager().list()
 
         #
         super().__init__(device)
 
         return
-
-    def _initialize_image_buffer(self, device):
-        """
-        this method determines the size of the image buffer on instantiation
-        """
-
-        #
-        try:
-            system  = PySpin.System.GetInstance()
-            cameras = system.GetCameras()
-            if type(device) == str:
-                camera = cameras.GetBySerial(device)
-            else:
-                camera = cameras.GetByIndex(device)
-            camera.Init()
-            w = camera.Width.GetMax()
-            h = camera.Height.GetMax()
-            camera.DeInit()
-            size = int(w * h)
-
-            #
-            del w; del h
-            del camera
-            cameras.Clear()
-            del cameras
-            system.ReleaseInstance()
-            del system
-
-            return mp.Array('i', size)
-
-        # NOTE - If the above code block fails, the buffer size defaults to 25
-        # million pixels which should support up to the 244S8 Blackfly S USB3
-        except PySpin.SpinnakerException:
-            return mp.Array('i', 25 * 1000000)
 
 class VideoStream(MainProcess):
     """
@@ -69,7 +32,7 @@ class VideoStream(MainProcess):
         """
 
         super().__init__(device)
-        self._spawn_child_process()
+
         self.open()
 
         return
@@ -80,20 +43,15 @@ class VideoStream(MainProcess):
 
         # spawn a child process as needed
         if self._child is None:
-            self._spawn_child_process()
+            self._spawn_child_process(StreamingChildProcess)
         else:
             raise CameraError('Video stream is already open')
 
         # set the acquisition flag
         self._child.acquiring.value = 1
 
+        #
         def f(child, camera, **kwargs):
-
-            # unpack the shape of the image
-            width, height = kwargs['shape']
-
-            # size of the image (i.e., total number of pixels)
-            size = width * height
 
             try:
                 camera.BeginAcquisition()
@@ -103,18 +61,19 @@ class VideoStream(MainProcess):
 
                     try:
                         image = camera.GetNextImage(kwargs['timeout'])
+
+                        #
+                        if not image.IsIncomplete():
+
+                            # convert the image
+                            data = image.Convert(PySpin.PixelFormat_Mono8, PySpin.HQ_LINEAR).GetNDArray().flatten()
+
+                            # store the image (critical - use lock)
+                            with child.buffer.get_lock():
+                                child.buffer[:] = data
+
                     except PySpin.SpinnakerException:
                         continue
-
-                    #
-                    if not image.IsIncomplete():
-
-                        # convert the image
-                        data = image.Convert(PySpin.PixelFormat_Mono8, PySpin.HQ_LINEAR).GetNDArray().flatten()
-
-                        # store the image (critical - use lock)
-                        with obj.buffer.get_lock():
-                            obj.buffer[:size] = data
 
                 return True, None
 
@@ -137,7 +96,7 @@ class VideoStream(MainProcess):
 
         # return if there is no active child or the stream is already closed
         if self._child is None:
-            raise CameraError('Video stream is not open')
+            raise CameraError('Video stream is already closed')
 
         # unset the acquisition flag
         self._child.acquiring.value = 0
@@ -174,15 +133,12 @@ class VideoStream(MainProcess):
         if self._child is None:
             raise CameraError('Video stream is closed')
 
-        # grab the image currently loaded into the buffer
+        # grab the image most recently buffered image
         try:
-
-            # acquire the buffer object's lock
             with self._child.buffer.get_lock():
-                data = self._child.buffer[:][:self.width * self.height]
-                image = np.array(data, dtype=np.uint8).reshape([self.height, self.width])
-
+                data = self._child.buffer[:]
+            image = np.array(data, dtype=np.uint8).reshape([self.height, self.width])
             return (True, image)
 
-        else:
+        except:
             return (False, None)
